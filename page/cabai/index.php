32206910fe60afa5352928acee35bdc6e1685118
setiamuktiazizah <setiamuktiazizah3@student.uns.ac.id>
2023-06-15 21:46:39 +0700

    edit nama web menjadi  project KKN

--- page/cabai/index.php
+++ page/cabai/index.php
@@ -1,69 +1,69 @@
-<?php 
+<?php
 
-  if (isset($_GET['delete'])) {
-    $id = $_GET['id'];
-    $sql = "delete from cabai where id_cabai='$id'";
-    $query = mysqli_query($con, $sql);
-    if ($query) {
-      echo "<script>alert('Data berhasil dihapus!');window.location.href='index.php?p=cabai'</script>";
-    } else {
-      echo mysqli_error($con);
-    }
+if (isset($_GET['delete'])) {
+  $id = $_GET['id'];
+  $sql = "delete from cabai where id_cabai='$id'";
+  $query = mysqli_query($con, $sql);
+  if ($query) {
+    echo "<script>alert('Data berhasil dihapus!');window.location.href='index.php?p=cabai'</script>";
+  } else {
+    echo mysqli_error($con);
   }
+}
 
- ?>
+?>
 
 <div class="row">
-    <div class="col-xs-12">
+  <div class="col-xs-12">
 
-      <div class="box">
-        <div class="box-header">
-          <h3 class="box-title">Data Table With Full Features</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-          <table id="example1" class="table table-bordered table-striped">
-            <thead>
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data Project KKN</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table id="example1" class="table table-bordered table-striped">
+          <thead>
             <tr>
               <th>Id</th>
               <th>Kriteria</th>
               <th>Dataran</th>
               <th>Aksi</th>
             </tr>
-            </thead>
-            <tbody>
-            	<?php 
+          </thead>
+          <tbody>
+            <?php
 
-            		$sql = "select * from cabai";
-            		$query = mysqli_query($con, $sql);
-            		while ($row = mysqli_fetch_assoc($query)):
+            $sql = "select * from cabai";
+            $query = mysqli_query($con, $sql);
+            while ($row = mysqli_fetch_assoc($query)) :
 
-            	 ?>
-            	 <tr>
-            	 	<td><?= $row['id_cabai'] ?></td>
-            	 	<td><?= $row['nama_cabai'] ?></td>
+            ?>
+              <tr>
+                <td><?= $row['id_cabai'] ?></td>
+                <td><?= $row['nama_cabai'] ?></td>
                 <td><?= $row['dataran'] ?></td>
                 <td>
                   <a href="index.php?p=cabai&act=edit&id=<?= $row['id_cabai'] ?>" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i></a>
                   <a href="index.php?p=cabai&delete&id=<?= $row['id_cabai'] ?>" class="btn btn-danger" onclick="return confirm('Apakah data akan dihapus?')"><i class="glyphicon glyphicon-trash"></i></a>
                 </td>
-            	 </tr>
-            	<?php endwhile; ?>
-            </tbody>
-            <tfoot>
+              </tr>
+            <?php endwhile; ?>
+          </tbody>
+          <tfoot>
             <tr>
               <th>Id</th>
               <th>Kriteria</th>
               <th>Dataran</th>
               <th>Aksi</th>
             </tr>
-            </tfoot>
-          </table>
-        </div>
-        <!-- /.box-body -->
+          </tfoot>
+        </table>
       </div>
-      <!-- /.box -->
+      <!-- /.box-body -->
     </div>
-    <!-- /.col -->
+    <!-- /.box -->
   </div>
-  <!-- /.row -->
+  <!-- /.col -->
+</div>
+<!-- /.row -->
--- page/dashboard.php
+++ page/dashboard.php
@@ -1,18 +1,15 @@
-	
-	<?php if (@$_SESSION['logged'] == true): ?>
-		
-        <div class="callout callout-info">
-          <h4>Selamat datang <?= $_SESSION['name'] ?></h4>
+	<?php if (@$_SESSION['logged'] == true) : ?>
 
-          <p>Sistem penunjang keputusan varietas bibit cabai kriting terbaik dengan menerapkan metode Fuzzy AHP</p>
-        </div>
-    <?php else: ?>
+	  <div class="callout callout-info">
+	    <h4>Selamat datang <?= $_SESSION['name'] ?></h4>
 
-		<div class="callout callout-info">
-          <h4>Fuzzy AHP</h4>
+	    <p>Sistem Pendukung Keputusan Projet KKN dengan Metode Fuzzy AHP</p>
+	  </div>
+	<?php else : ?>
 
-          <p>Sistem penunjang keputusan varietas bibit cabai kriting terbaik dengan menerapkan metode Fuzzy AHP</p>
-        </div>
-    <?php endif; ?>
+	  <div class="callout callout-info">
+	    <h4>SPK Project KKN</h4>
 
-        
+	    <p>Sistem Pendukung Keputusan dengan Metode Fuzzy AHP</p>
+	  </div>
+	<?php endif; ?>
