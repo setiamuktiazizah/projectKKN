7abcbfd4d3499a7875fcdcc7fb0e45121015d783
setiamuktiazizah <setiamuktiazizah3@student.uns.ac.id>
2023-06-22 16:34:54 +0700

    fix database

--- spkkkn.sql
+++ spkkkn.sql
@@ -52,8 +52,20 @@ CREATE TABLE `project` (
 
 INSERT INTO `project` (`id_project`, `nama_project`, `bidang`) VALUES
 (1, 'Penyuluhan terkait isu kesehatan (kebersihan, pola makan sehat, pencegahan penyakit, pentingnya imunisasi)', 'kesehatan'),
-(2, 'Kegiatan bimbingan belajar dalam membantu siswa memahami pelajaran dan meningkatkan kemampuan akademik mereka dengan menerapkan metode pembelajaran yang inovatif', 'pendidikan'),
-(3, 'Pelatihan dasar-dasar komputer (ms.office)', 'teknologi');
+(2, 'Pemeriksaan kesehatan gratis (pemeriksaan tekanan darah, gula darah, pengukuran berat badan, dsb)', 'kesehatan'),
+(3, 'Pelatihan pertolongan pertama kepada masyaratakt, termasuk penanganan luka, pernapasan buatan, pendarahan, dan pengelolaan trauma', 'kesehatan'),
+(4, 'Kegiatan bimbingan belajar dalam membantu siswa memahami pelajaran dan meningkatkan kemampuan akademik mereka dengan menerapkan metode pembelajaran yang inovatif', 'pendidikan'),
+(5, 'Penyuluhan pentingnya pendidikan bagi anak ', 'pendidikan'),
+(6, 'Pelatihan guru untuk meningkatkan keterampilan dan pengetahuan guru dalam pengajaran dan manajemen kelas', 'pendidikan'),
+(7, 'Pelatihan dasar-dasar komputer (ms.office)', 'teknologi'),
+(8, 'Membangun aplikasi atau web yang dibutuhkan di desa', 'teknologi'),
+(9, 'Membantu memperbaiki atau membangun sarana air bersih, seperti sumur bor atau instalasi air bersih sederhana', 'lingkungan'),
+(10, 'Mengadakan kegiatan kerja bakti lingkungan', 'lingkungan'),
+(11, 'Membuat jalur mitigasi bencana', 'lingkungan'),
+(12, 'Mengadakan kegiatan penyuluhan dan pelatihan kepada masyarakat tentang mitigasi bencana, termasuk pengetahuan tentang risiko bencana, identifikasi bahaya dan langkah pencegahan yang dapat diambil', 'lingkungan'),
+(13, 'Mengadakan kegiatan daur ulang sampah menjadi barang yang berguna', 'lingkungan'),
+(14, 'Penyuluhan mengenai teknik bercocok tanam, kondisi tanaman yang sesuai berdasarkan jenis tanah, iklim, dll.', 'pertanian'),
+(15, 'Pelatihan mengenai pembuatan dan perawatan tanaman hidroponik', 'pertanian');
 
 -- --------------------------------------------------------
 
@@ -76,12 +88,25 @@ INSERT INTO `desc_kriteria` (`id_desc`, `id_kriteria`, `deskripsi`, `nilai`) VAL
 (1, 1, 'mudah (bisa dilalui kendaraan besar, hampir semua jalan diaspal)', 1),
 (2, 1, 'sedang (bisa dilalui roda 4 dan roda 2 tapi tidak bisa dilalui kendaraan besar, tidak semua jalan diaspal', 3),
 (3, 1, 'sulit (hanya bisa dilalui roda 2, hampir seluruh jalan tidak diaspal)', 9),
-(4, 2, 'sedikit', 7),
-(5, 2, 'sedang', 4),
-(6, 2, 'banyak', 2),
+(4, 2, 'sedikit (tingkat ketersediaan air bersih terbatas)', 7),
+(5, 2, 'sedang (ketersediaan air cukup untuk memenuhi kebutuhan dasar populasi)', 4),
+(6, 2, 'banyak (tersedia sumber air yang melimpah)', 2),
 (7, 3, 'dekat (<10 km)', 2),
 (8, 3, 'sedang (10-50 km)', 5),
-(9, 3, 'jauh (>50 km)', 7);
+(9, 3, 'jauh (>50 km)', 7),
+(10, 4, 'tidak rawan', 2),
+(11, 4, 'rawan sedang', 5),
+(12, 4, 'rawan tinggi', 8),
+(13, 5, 'tidak ada akses internet', 9),
+(14, 5, 'tidak stabil dan jangkauan terbatas', 7),
+(15, 5, 'cepat dan stabil', 3),
+(16, 6, 'rendah', 6),
+(17, 6, 'sedang', 5),
+(18, 6, 'tinggi', 3),
+(19, 7, 'baik (akses terhadap fasilitas kesehatan, sanitasi memadai)', 2),
+(20, 7, 'cukup baik', 4),
+(21, 7, 'kurang baik', 7),
+(22, 7, 'buruk', 9);
 
 -- --------------------------------------------------------
 
@@ -101,7 +126,11 @@ CREATE TABLE `kriteria` (
 INSERT INTO `kriteria` (`id_kriteria`, `kriteria`) VALUES
 (1, 'Sarana transportasi'),
 (2, 'Sumber air'),
-(3, 'Jarak desa ke pusat kota');
+(3, 'Jarak desa ke pusat kota'),
+(4, 'Rawan bencana'),
+(5, 'Akses internet'),
+(6, 'Tingkat pendidikan'),
+(7, 'Tingkat kesehatan');
 
 -- --------------------------------------------------------
 
@@ -136,14 +165,54 @@ CREATE TABLE `nilai_kriteria` (
 
 INSERT INTO `nilai_kriteria` (`id_nilai`, `id_kriteria`, `baris`, `kolom`, `nilai`) VALUES
 (1, 1, 1, 1, 1),
-(2, 1, 1, 2, 0.2),
-(3, 1, 1, 3, 0.143),
-(4, 2, 2, 2, 0.33),
-(5, 2, 2, 3, 1),
-(6, 3, 3, 3, 1),
-(7, 2, 2, 1, 5),
-(8, 3, 3, 1, 7),
-(9, 3, 3, 2, 3);
+(2, 1, 1, 2, 2),
+(3, 1, 1, 3, 3),
+(4, 1, 1, 4, 1),
+(5, 1, 1, 5, 2),
+(6, 1, 1, 6, 3),
+(7, 1, 1, 7, 4),
+(8, 2, 2, 2, 1),
+(9, 2, 2, 3, 2),
+(10, 2, 2, 4, 3),
+(11, 2, 2, 5, 4),
+(12, 2, 2, 6, 6),
+(13, 2, 2, 7, 8),
+(14, 3, 3, 3, 1),
+(15, 3, 3, 4, 2),
+(16, 3, 3, 5, 5),
+(17, 3, 3, 6, 7),
+(18, 3 ,3 ,7, 9),
+(19, 4, 4, 4, 1),
+(20, 4, 4, 5, 3),
+(21, 4, 4, 6, 4),
+(22, 4, 4, 7, 1),
+(23, 5, 5, 5, 1),
+(24, 5, 5, 6, 5),
+(25, 5, 5, 7, 7),
+(26, 6, 6, 6, 1),
+(27, 6, 6, 7, 9),
+(28, 7, 7, 7, 1),
+(29, 2, 2, 1, 0.5),
+(30, 3, 3, 1, 0.333),
+(31, 3, 3, 2, 0.5),
+(32, 4, 4, 1, 1),
+(33, 4, 4, 2, 0.333),
+(34, 4, 4, 3, 0.5),
+(35, 5, 5, 1, 0.5),
+(36, 5, 5, 2, 0.25),
+(37, 5, 5 ,3, 0.2),
+(38, 5, 5, 4, 0.333),
+(39, 6, 6, 1, 0.333),
+(40, 6, 6, 2, 0.166),
+(41, 6, 6, 3, 0.143),
+(42, 6, 6, 4, 0.25),
+(43, 6, 6, 5, 0.2),
+(44, 7, 7, 1, 0.25),
+(45, 7, 7, 2, 0.125),
+(46, 7, 7, 3, 0.111),
+(47, 7, 7, 4, 1),
+(48, 7, 7, 5, 0.143),
+(49, 7, 7, 6, 0.111);
 
 -- --------------------------------------------------------
 
